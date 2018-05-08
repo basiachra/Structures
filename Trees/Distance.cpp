dbb9d02a59d1c37f00140ee11172b3c1e5af1d9d
bielakarolina <bielakarolina77@gmail.com>
2018-05-08 19:14:18 +0200

    Wake me up before you

--- Trees/Distance.cpp
+++
@@ -1,6 +0,0 @@
-//
-// Created by Karolina on 05.05.2018.
-//
-//https://www.geeksforgeeks.org/find-distance-between-two-nodes-of-a-binary-tree/
-//Find the distance between two keys in a binary tree, no parent pointers are given.
-//Distance between two nodes is the minimum number of edges to be traversed to reach one node from other.
